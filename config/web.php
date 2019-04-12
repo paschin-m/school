81bf14440b695c720729a9d325671bf283d8b0aa
m.pahin <m.pachin@roseltorg.ru>
2019-04-12 21:00:52 +0300

    master commit add controller tests

--- config/web.php
+++ config/web.php
@@ -1,29 +1,7 @@
 <?php
 
 
-  return $config=[
+  return [
       'id'=>'school',
-      'basePath' =>realpath(__DIR__.'/../'),
-      'components' => [
-        'assetManager' => [
-                          'class' => 'yii\web\AssetManager',
-                          'appendTimestamp' => true,
-        'bundles' => [
-                'romdim\bootstrap\material\BootMaterialCssAsset' => [
-                                                                    'css' => [
-                                                                              YII_ENV_DEV ? 'css/ripples.css' : 'css/ripples.min.css',
-                                                                              YII_ENV_DEV ? 'css/material.css' : 'css/material.min.css',
-                                                                            ]
-                                                                    ],
-                'romdim\bootstrap\material\BootMaterialJsAsset' => [
-                                                                    'js' => [
-                                                                        YII_ENV_DEV ? 'js/ripples.js' : 'js/ripples.min.js',
-                                                                        YII_ENV_DEV ? 'js/material.js' : 'js/material.min.js',
-                                                                    ]
-                                                                  ]
-                    ]
-                  ]
-
-                ]
-
+      'basePath' =>realpath(__DIR__.'/../')
   ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,5 @@
 <?php
     use yii\bootstrap\NavBar;
-
 ?>
 <html>
 <head>
