f2d11889be432f8faff05d4821171af9eda87298
m.pahin <m.pachin@roseltorg.ru>
2019-04-15 17:37:08 +0300

    master commit add controller tests add navbar test На USerJoin

--- tests/_support/Step/Acceptance/TestUserJoin.php
+++ tests/_support/Step/Acceptance/TestUserJoin.php
@@ -6,32 +6,53 @@ class TestUserJoin extends \AcceptanceTester
 
     public function imagineUser()
     {
-        $I = $this;
+        $user=[
+            "name" => "John",
+            "email" => "admin@local.ru",
+            "password"=>"123456"
+        ];
+        print_r($user);
+        return $user;
     }
 
-    public function joinUser()
+    public function joinUser($user)
     {
         $I = $this;
+        $I->amOnPage("user/join");
+        $I->see("Join us");
+        $I->fillField("UserJoinForm[name]",$user["name"]);
+        $I->fillField("UserJoinForm[email]",$user["email"]);
+        $I->fillField("UserJoinForm[password]",$user["password"]);
+        $I->fillField("UserJoinForm[password2]",$user["password2"]);
     }
 
-    public function loginUser()
+    public function loginUser($user)
     {
         $I = $this;
+        $I->amOnPage("user/login");
+        $I->see("Log in");
+        $I->fillField("UserJoinForm[email]",$user["email"]);
+        $I->fillField("UserJoinForm[password]",$user["password"]);
+        $I->click("Enter");
     }
 
     public function logoutUser()
     {
         $I = $this;
+        $I->click('Logout');
+
     }
 
-    public function isUserLogged()
+    public function isUserLogged($user)
     {
         $I = $this;
+        $I->see($user['name']);
     }
 
-    public function noUserLogged()
+    public function noUserLogged($user)
     {
         $I = $this;
+        $I->dontSee($user['name']);
     }
 
 }
--- tests/acceptance/TestUserJoinCept.php
+++ tests/acceptance/TestUserJoinCept.php
@@ -1,3 +1,33 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
+<?php
+use Step\Acceptance\TestUserJoin;
+
+$I = new TestUserJoin($scenario);
+$I->wantTo('New users join and logn');
+
+$user1=$I->imagineUser();
+$user2=$I->imagineUser();
+
+$I->loginUser($user1);
+$I->see("Такого пользователя нет");
+
+$I->joinUser($user1);
+$I->joinUser($user2);
+
+$I->joinUser($user1);
+$I->see("пользователь уже существует");
+
+$I->loginUser($user1);
+$I->isUserLOgged($user1);
+$I->noUserLogged($user2);
+$I->logoutUser();
+
+$I->loginUser($user2);
+$I->isUserLOgged($user2);
+$I->noUserLogged($user1);
+$I->logoutUser();
+
+
+$user1["password"]="errorpass";
+
+$I->loginUser($user1);
+$I->see('Неверный пароль пользователя');
