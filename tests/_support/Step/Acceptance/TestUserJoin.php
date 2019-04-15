1b63ed4956f2691f7213a1ef3442064f88a55e48
m.pahin <m.pachin@roseltorg.ru>
2019-04-15 17:13:49 +0300

    master commit add controller tests add navbar test На USerJoin

---
+++ tests/_support/Step/Acceptance/TestUserJoin.php
@@ -0,0 +1,37 @@
+<?php
+namespace Step\Acceptance;
+
+class TestUserJoin extends \AcceptanceTester
+{
+
+    public function imagineUser()
+    {
+        $I = $this;
+    }
+
+    public function joinUser()
+    {
+        $I = $this;
+    }
+
+    public function loginUser()
+    {
+        $I = $this;
+    }
+
+    public function logoutUser()
+    {
+        $I = $this;
+    }
+
+    public function isUserLogged()
+    {
+        $I = $this;
+    }
+
+    public function noUserLogged()
+    {
+        $I = $this;
+    }
+
+}
