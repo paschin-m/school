1cf762e13854e5631b2917d38df9f2c3e5ff014c
m.pahin <m.pachin@roseltorg.ru>
2019-04-16 12:49:34 +0300

    master commit add controller tests add navbar test На USerJoin UserIdentity Форма валидации данных

--- models/UserJoinForm.php
+++ models/UserJoinForm.php
@@ -10,4 +10,18 @@ class UserJoinForm extends Model
   public $email;
   public $password;
   public $password2;
+
+  public function rules()
+  {
+    return [
+        ['name','required','message'=>'обязательное поле введите данные'],
+        ['email','required','message'=>'обязательное поле введите данные'],
+        ['password','required','message'=>'обязательное поле введите данные'],
+        ['password2','required','message'=>'обязательное поле введите данные'],
+        ['name','string', 'min'=>3,'max'=>45,'message'=>'вы превысили лимит, возможно не бывает таких длинных имен!'],
+        ['email','email','message'=>'Здесь адре электронной почты, а не ВСЯКАЯХЕРНЯ!'],
+        ['password','string','min'=>4],
+        ['password2','compare','compareAttribute'=>'password','message'=>'Пароли не совпадают']
+    ];
+  }
 }
