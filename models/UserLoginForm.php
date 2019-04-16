d9c0b1408f332e58e03eee13a5b25de649d03354
m.pahin <m.pachin@roseltorg.ru>
2019-04-16 19:31:06 +0300

    master commit add controller tests add navbar test На USerJoin UserIdentity Форма валидации данных

--- models/UserLoginForm.php
+++ models/UserLoginForm.php
@@ -9,30 +9,40 @@ class UserLoginForm extends Model
   public $email;
   public $password;
 
+  private $userRecord;
+
   public function rules()
   {
     return [
             ['email','required','message'=>'поле не может быть пустым'],
             ['password','required','message'=>'поле не может быть пустым'],
             ['email','email','message'=>'совсем охуел человек ебаный!'],
-            ['email','errorIfEmailNotFound'] #собственная проверка почты, а что если нет никакой почты!?
+            ['email','errorIfEmailNotFound'], #собственная проверка почты, а что если нет никакой почты!?
+            ['password','errorIfPasswordWrong','message'=>'не верный пароль для входа']
         ];
   }
 
   #реализация валидации поиска по почте
   public function errorIfEmailNotFound()
   {
-    $userRecord=UserRecord::findUserByEmail($this->email);
-    if($userRecord->email!=$this->email)
+    $this->userRecord =UserRecord::findUserByEmail($this->email);
+    if($this->userRecord==null)
       $this->addError("email", "Пользователь с таким почтовым ящиком не зарегистрирован");
+  }
+
+  public function errorIfPasswordWrong()
+  {
+    if($this->hasErrors())
+      return ;
+    if ($this->userRecord->passhash!=$this->password)
+      $this->addError('password','Не верный пароль пользователя');
   }
 
   public function login()
   {
     if($this->hasErrors())
       return ;
-    $userRecord=UserRecord::findUserByEmail($this->email);
-    $userIdentity=UserIdentity::findIdentity($userRecord->id);
+    $userIdentity=UserIdentity::findIdentity($this->userRecord->id);
     Yii::$app->user->login($userIdentity);
   }
 }
